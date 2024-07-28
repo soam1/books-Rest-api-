8c79d3daeb12bb1b01ed254baebfc8e7dc0f370e
soam1 <soamakash9@gmail.com>
2024-07-28 12:05:30 +0530

    file uploading | MultipartFile

---
+++ src/main/java/com/akashsoam/bootJpaExample/controller/FileUploadController.java
@@ -0,0 +1,45 @@
+package com.akashsoam.bootJpaExample.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.akashsoam.bootJpaExample.helper.FileUploadHelper;
+
+@RestController
+public class FileUploadController {
+	
+	@Autowired
+	private FileUploadHelper fileUploadHelper;
+	
+	@PostMapping("/upload_file")
+	public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+//		// upload file code
+//		System.out.println(file.getOriginalFilename());
+//		System.out.println(file.getSize());
+//		System.out.println(file.getContentType());
+//		System.out.println(file.getName());
+		
+		if (file.isEmpty()) {
+			return ResponseEntity.status(500).body("File is empty");
+		}
+		
+		if (!file.getContentType().equals("image/jpeg") && !file.getContentType().equals("image/png")) {
+			return ResponseEntity.status(500).body("Only jpeg or png files are allowed");
+		}
+		
+		// upload the file
+		try {
+			if (fileUploadHelper.uploadFile(file)) {
+				return ResponseEntity.ok("File is uploaded");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return ResponseEntity.status(500).body("Something went wrong");
+
+	}
+
+}
---
+++ src/main/java/com/akashsoam/bootJpaExample/helper/FileUploadHelper.java
@@ -0,0 +1,26 @@
+package com.akashsoam.bootJpaExample.helper;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+@Component
+public class FileUploadHelper {
+
+	public final String UPLOAD_DIR = "/home/akash-soam/software-workspaces/sts-workspace/BootRestBook/src/main/resources/static/images";
+
+	public boolean uploadFile(MultipartFile file) {
+		boolean f = false;
+		try {
+			Files.copy(file.getInputStream(), Paths.get(UPLOAD_DIR + File.separator + file.getOriginalFilename()),
+					StandardCopyOption.REPLACE_EXISTING);
+			f = true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return f;
+	}
+}
